5cbd0c8d43d3ba0e172af09c9f977ea9f1ee75e8
Priyariya0825 <146413107+Priyariya0825@users.noreply.github.com>
2023-09-29 11:21:47 +0530

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,4 @@
+totalMoney = int(input("How much money do you earn in £?"'\n'))
+shares = int(input("How many people do you share your money with?"'\n'))
+lessMoney = totalMoney / shares 
+print("Each person you share your money with get £",lessMoney )
